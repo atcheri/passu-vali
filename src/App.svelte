dd4805b1e90ede172860bd33dbae885934a00d5b
Atcheri Atch <atcheri@gmail.com>
2023-01-08 23:19:35 +0100

    feat: main application styling

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,28 @@
   import Validator from "./lib/Validator.svelte";
 </script>
 
-<main>
-  <h1>Passu-Vali. But not really</h1>
-  <Validator />
+<main class="flex justify-center items-center container mx-auto h-screen">
+  <div class="flex flex-col-reverse md:flex-row gap-6 items-center">
+    <div
+      class="flex flex-wrap content-center justify-center rounded-r-md"
+      style="width: 24rem; height: 32rem;"
+    >
+      <img
+        class="w-full h-full bg-center bg-no-repeat bg-cover"
+        src="/password.svg"
+        alt="is your password secure enough?"
+      />
+    </div>
+    <div>
+      <div class="my-6">
+        <h1 class="text-center text-3xl text-indigo-600">
+          Is your Passu-Vali?
+        </h1>
+        <h2 class="text-center text-xl text-indigo-400">Or not really</h2>
+      </div>
+      <Validator />
+    </div>
+  </div>
 </main>
 
 <style>
--- src/lib/Validator.svelte
+++ src/lib/Validator.svelte
@@ -6,13 +6,26 @@
 </script>
 
 <form on:submit|preventDefault={validate}>
-  <input
-    {type}
-    value={get(password)}
-    name="password"
-    id="password"
-    on:input={handleInput}
-  />
-  <button type="submit">Submit</button>
-  <Rules />
+  <div class="mb-3">
+    <label for="password" class="mb-2 block text-xs font-semibold"
+      >Enter your <b>Strong</b> Password</label
+    >
+    <input
+      {type}
+      value={get(password)}
+      name="password"
+      id="password"
+      on:input={handleInput}
+      placeholder="*****"
+      class="block w-full rounded-md border border-gray-300 focus:border-purple-700 focus:outline-none focus:ring-1 focus:ring-purple-700 py-1 px-1.5 text-gray-500"
+    />
+  </div>
+  <button
+    type="submit"
+    class="block w-full text-center text-white duration-200 bg-indigo-700 hover:bg-indigo-900 px-2 py-1.5 rounded-md"
+    >Validate</button
+  >
+  <div class="mt-4">
+    <Rules />
+  </div>
 </form>
